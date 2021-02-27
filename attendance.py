8fe51a8a5d2d8df3aa277934a3abfe9355255785
Jordyn <34407529+jordynmay@users.noreply.github.com>
2021-02-27 16:47:19 -0600

    Add files via upload

---
+++ attendance.py
@@ -0,0 +1,29 @@
+import xlwt
+import sys
+
+def main() -> None:
+    inFile = sys.argv[1]
+    outFile = sys.argv[2]
+    numStudents
+    
+    inputF = open(inFile, "r")
+    for line in inputF.readlines():
+        tempStr = line.rstrip("\n")
+        tempList = tempStr.spli(",")
+        name = tempList[0]
+        nameList.append(name)
+    inputF.close()
+
+    nameList.pop()
+
+    wb = Workbook()
+
+    for i in range(1, numStudents+1):
+        #
+    
+
+    
+
+
+if __name__ == "__main__":
+    main()
